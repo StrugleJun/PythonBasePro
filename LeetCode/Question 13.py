da3806eb7412b1d74e2c4192b16e1950d9a8a1ae
君 <strugle.jun@hotmail.com>
2024-12-25 23:43:07 +0800

    Question 13

---
+++ LeetCode/Question 13.py
@@ -0,0 +1,84 @@
+"""
+13. 罗马数字转整数
+https://leetcode.cn/problems/roman-to-integer/description/
+
+罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+
+字符          数值
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+
+通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+
+I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+给定一个罗马数字，将其转换成整数。
+
+示例 1:
+
+输入: s = "III"
+输出: 3
+示例 2:
+
+输入: s = "IV"
+输出: 4
+示例 3:
+
+输入: s = "IX"
+输出: 9
+示例 4:
+
+输入: s = "LVIII"
+输出: 58
+解释: L = 50, V= 5, III = 3.
+示例 5:
+
+输入: s = "MCMXCIV"
+输出: 1994
+解释: M = 1000, CM = 900, XC = 90, IV = 4.
+"""
+
+
+class Solution:
+    SYMBOL_VALUES = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    def romanToInt(self, s: str) -> int:
+        """
+        灵魂思路：当前位置的元素比下个位置的元素小，就减去当前值，否则加上当前值
+        :param s:  III, IV, X, L, C, D, M
+        :return:
+        """
+        length = len(s)
+        answer = 0
+        for index, symbol in enumerate(s):
+            if symbol in self.SYMBOL_VALUES:
+                value = self.SYMBOL_VALUES[symbol]
+                if index < length - 1 and value < self.SYMBOL_VALUES[s[index + 1]]:  # index + 1 会越界
+                    # 若存在小的数字在大的数字的左边的情况，根据规则需要减去小的数字。对于这种情况，我们也可以将每个字符视作一个单独的值，若一个数字右侧的数字比它大，则将该数字的符号取反。
+                    answer -= value
+                else:
+                    # 通常情况下，罗马数字中小的数字在大的数字的右边。若输入的字符串满足该情况，那么可以将每个字符视作一个单独的值，累加每个字符对应的数值即可。
+                    answer += value
+
+        return answer
+
+
+solution = Solution()
+print(solution.romanToInt("III"))
+print(solution.romanToInt("XIV"))
+print(solution.romanToInt("XXVII"))
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # PythonBasePro
 PythonPracticePro
+
+## Django
+>   基于Django实现个人博客系统
+
+Django官网: https://www.djangoproject.com/
+
+## LeetCode
+>   刷题
+### Question 1
+
+### Question 2
+
+## attr模块
+>   attr模块的使用
+
+## 依赖注入
+>   Python版的IOC和DI
+
+## 基础语法
+>   字面意思
+
+## 奇淫技巧
+>   研究各种魔法和便利
+
+## 数据分析
+>   Numpy和Pandas
+
+## 数据库操作
+>   字面意思
+
+## 进阶用法
+>   线程池
+
